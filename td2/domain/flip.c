1baeabc55778d68ae9ade205dc77cbc7b9859ddb
Antoine Dudouit <antoine.dudouit@bordeaux-inp.fr>
2021-03-01 21:20:57 +0100

    fixing valgrind flip.c issues

--- td2/domain/flip.c
+++ td2/domain/flip.c
@@ -4,66 +4,67 @@
 
 
 void
-one_process(char current_dir, pnm* ims, pnm* imd, int rows, int cols)
+one_process(char current_dir, char* ims_name, char* imd_name)
 {
-    for (int p = 0; p < rows; p++) {
-        for (int q = 0; q < cols; q++) {
+
+    pnm ims = pnm_load(ims_name);
+
+    int imd_rows, imd_cols;
+    
+    int ims_rows = pnm_get_height(ims);
+    int ims_cols = pnm_get_width(ims);
+
+    if(current_dir == 'h' || current_dir == 'v' ){
+        imd_rows = ims_rows;
+        imd_cols = ims_cols;
+    }
+    else if(current_dir == 't'){
+        imd_rows = ims_cols;
+        imd_cols = ims_rows;
+    }
+    else{
+        return;
+    }
+
+    pnm imd = pnm_new(imd_cols, imd_rows, PnmRawPpm);
+
+    for (int p = 0; p < ims_rows; p++) {
+        for (int q = 0; q < ims_cols; q++) {
             for (int k = 0; k < 3; k++) {
                 float val;
                 if(current_dir == 'h') {
-                    val = pnm_get_component(*ims, p, cols-1-q, k);
-                    pnm_set_component(*imd, p, q, k, val);
-
+                    val = pnm_get_component(ims, p, ims_cols-1-q, k);
+                    pnm_set_component(imd, p, q, k, val);
                 }
                 if(current_dir == 'v') {
-                    val = pnm_get_component(*ims, rows-1-p, q, k);
-                    pnm_set_component(*imd, p, q, k, val);
+                    val = pnm_get_component(ims, ims_rows-1-p, q, k);
+                    pnm_set_component(imd, p, q, k, val);
                 }
                 if(current_dir == 't') {
-                    val = pnm_get_component(*ims, p, q, k);
-                    pnm_set_component(*imd, q, p, k, val);
+                    val = pnm_get_component(ims, p, q, k);
+                    pnm_set_component(imd, q, p, k, val);
                 }
 
             }
         }
-    }
-}
-
-void 
-process(char* dir, char* ims_name, char* imd_name)
-{
-    pnm ims = pnm_load(ims_name);
-    pnm imd = pn;
-
-    int rows = pnm_get_height(ims);
-    int cols = pnm_get_width(ims);
-
-    int i = 0;
-    while(dir[i] != '\0'){
-        if(dir[i] == 'h' || dir[i] == 'v' ){
-            pnm imd_tmp = pnm_new(cols, rows, PnmRawPpm);
-            one_process(dir[i], &ims, &imd_tmp, rows, cols);
-            imd = pnm_dup(imd_tmp);
-            free(imd_tmp);
-        }
-        else if(dir[i] == 't'){
-            pnm imd_tmp = pnm_new(rows, cols, PnmRawPpm);
-            one_process(dir[i], &ims, &imd_tmp, rows, cols);
-            imd = pnm_dup(imd_tmp);        
-            free(imd_tmp);
-        }
-        else{
-            break;
-        }
-        i++;
     }
 
     pnm_save(imd, PnmRawPpm, imd_name);
 
     pnm_free(ims);
     pnm_free(imd);
+}
 
-
+void 
+process(char* dir, char* ims_name, char* imd_name)
+{
+    int i = 0;
+    char * tmp_ims_name = ims_name;
+    while(dir[i] != '\0'){
+        one_process(dir[i], tmp_ims_name, imd_name);
+        tmp_ims_name = imd_name;
+        i++;
+    }
 }
 
 void

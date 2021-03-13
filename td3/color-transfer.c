df64b5cf2e6117e8c0a867bff6a6f6e7cbe6ee7e
sstan001 <>
2021-03-13 20:21:59 +0100

    rgb to lms and lms to rgb ok

--- td3/color-transfer.c
+++ td3/color-transfer.c
@@ -5,6 +5,9 @@
  *        Erik Reinhard, Michael Ashikhmin, Bruce Gooch and Peter Shirley, 
  *        'Color Transfer between Images', IEEE CGA special issue on 
  *        Applied Perception, Vol 21, No 5, pp 34-41, September - October 2001
+ *
+ * Link for the article which inspired the following exercise:
+ * https://vta.vvv.enseirb-matmeca.fr/IT220/20.21/RAGS-01.pdf
  */
 
 #include <float.h>
@@ -40,67 +43,124 @@ float LAB2RGB[D][D] = {
         {0.5774, -0.8165, 0}
 };
 
-/*
-void rgb2lab(void *** ims, int rows_ims, int cols_ims){
-
-    for(int i = 0 ; i < rows_ims ; i++){
-        for(int j = 0 ; j < cols_ims ; j++){
-            for(int k = 0 ; 1 < 3 ; k++){
-                ims[i][j][k] = 0;
-            }
-        }
-    }   
-}
-
-void lab2rgb(pnm ims){
-    ims;
-}
-*/
-
-void
-process(char* ims_name, char* imt_name, char* imd_name) {
-    pnm ims = pnm_load(ims_name);
-    int rows_ims = pnm_get_height(ims);
-    int cols_ims = pnm_get_width(ims);
-
-
-    //float* ims_data = malloc(sizeof(float) * rows_ims * cols_ims * 3);
-    //float (* ims_col)[rows_ims] = (float (*)[rows_ims]) ims_data;
-
-    int*** data = (int***) malloc(rows_ims * sizeof(int*));
-    for (int i = 0; i < rows_ims; i++) {
-        data[i] = (int**) malloc(cols_ims * sizeof(int*));
-        for (int j = 0; j < cols_ims; j++) {
-            data[i][j] = (int*) malloc(3 * sizeof(int));
+// Allocates memory for a 3D matrix of floats and dimensions rows * cols * 3
+float*** malloc_img(int rows, int cols) {
+    float*** data = (float***) malloc(rows * sizeof(float*));
+    for (int i = 0; i < rows; i++) {
+        data[i] = (float**) malloc(cols * sizeof(float*));
+        for (int j = 0; j < cols; j++) {
+            data[i][j] = (float*) malloc(3 * sizeof(float));
             for (int k = 0; k < 3; k++) {
                 data[i][j][k] = 0;
             }
         }
     }
+    return data;
+}
 
-
-
-
-    for (int i = 0; i < rows_ims; i++) {
-        for (int j = 0; j < cols_ims; j++) {
+// Frees the memory allocated, and pointed to by data
+void free_img(int rows, int cols, float*** data) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             free(data[i][j]);
         }
         free(data[i]);
     }
     free(data);
+}
+
+// Fills data with the content of ims
+void fill_img(int rows, int cols, float*** data, pnm ims) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            for (int k = 0; k < 3; k++) {
+                data[i][j][k] = pnm_get_component(ims, i, j, k);
+            }
+        }
+    }
+}
+
+// Computes the matrix product: res = matrix * channels
+void matrix_prod_channels(float matrix[3][3], float channels[3], float res[3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            res[i] += matrix[i][j] * channels[j];
+        }
+    }
+}
+
+// Rounds all values of data + truncates if it is out of bound
+void uniform(int rows, int cols, float*** data) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            for (int k = 0; k < 3; k++) {
+                data[i][j][k] = round(data[i][j][k]);
+                if (data[i][j][k] > 255) {
+                    data[i][j][k] = 255;
+                } else if (data[i][j][k] < 0) {
+                    data[i][j][k] = 0;
+                }
+            }
+        }
+    }
+}
+
+void process(char* ims_name, char* imt_name, char* imd_name) {
+    pnm ims = pnm_load(ims_name);
+    int rows_ims = pnm_get_height(ims);
+    int cols_ims = pnm_get_width(ims);
+    pnm imd = pnm_new(rows_ims, cols_ims, PnmRawPpm);
+
+    //float* ims_data = malloc(sizeof(float) * rows_ims * cols_ims * 3);
+    //float (* ims_col)[rows_ims] = (float (*)[rows_ims]) ims_data;
+
+    float*** data_ims = malloc_img(rows_ims, cols_ims);
+    fill_img(rows_ims, cols_ims, data_ims, ims);
+
+
+    // RGB to LMS
+    float*** data_ims_lms = malloc_img(rows_ims, cols_ims);
+    for (int i = 0; i < rows_ims; i++) {
+        for (int j = 0; j < cols_ims; j++) {
+            matrix_prod_channels(RGB2LMS, data_ims[i][j], data_ims_lms[i][j]);
+        }
+    }
+
+    // LMS goes back to RGB
+    float*** data_ims_rgb = malloc_img(rows_ims, cols_ims);
+    for (int i = 0; i < rows_ims; i++) {
+        for (int j = 0; j < cols_ims; j++) {
+            matrix_prod_channels(LMS2RGB, data_ims_lms[i][j],
+                                 data_ims_rgb[i][j]);
+        }
+    }
+
+    // To compare the initial image with the one which travelled
+    for (int i = 0; i < rows_ims; i++) {
+        for (int j = 0; j < cols_ims; j++) {
+            for (int k = 0; k < 3; k++) {
+                pnm_set_component(imd, i, j, k, data_ims_rgb[i][j][k]);
+            }
+        }
+    }
+
+
+    pnm_save(imd, PnmRawPpm, imd_name);
+
+    free_img(rows_ims, cols_ims, data_ims_rgb);
+    free_img(rows_ims, cols_ims, data_ims_lms);
+    free_img(rows_ims, cols_ims, data_ims);
     pnm_free(ims);
 }
 
-void
-usage(char* s) {
+void usage(char* s) {
     fprintf(stderr, "Usage: %s <ims> <imt> <imd> \n", s);
     exit(EXIT_FAILURE);
 }
 
 #define PARAM 3
 
-int
-main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != PARAM + 1) usage(argv[0]);
     process(argv[1], argv[2], argv[3]);
     return EXIT_SUCCESS;

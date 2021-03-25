050d8bb250ca0f90768cbac0bbda94437b5df385
sstan001 <24721907+sstan001@users.noreply.github.com>
2021-03-25 13:27:55 +0100

    display: ok but normalization is to improve

--- td5/test-fft.c
+++ td5/test-fft.c
@@ -15,12 +15,13 @@
 #define ERROR 5
 #define NAME_SIZE 50
 
-/**
- * @brief test the forward and backward functions
- * @param char* name, the input image file name
- */
 
-// Fills imd with g_imd, and compares imd with ims
+/**
+ * @brief fills the output img with the res and compares it with the input img
+ * @param unsigned short* g_imd, the resulting channel
+ * @param pnm imd, the ouput image
+ * @param pnm ims, the input image
+ */
 void fill_and_compare(unsigned short* g_imd, pnm imd, pnm ims) {
     int rows = pnm_get_height(ims);
     int cols = pnm_get_width(ims);
@@ -40,6 +41,10 @@ void fill_and_compare(unsigned short* g_imd, pnm imd, pnm ims) {
     fprintf(stderr, "OK\n");
 }
 
+/**
+ * @brief test the forward and backward functions
+ * @param char* name, the input image file name
+ */
 void
 test_forward_backward(char* name) {
     fprintf(stderr, "test_forward_backward: ");
@@ -111,17 +116,21 @@ test_reconstruction(char* name) {
     fftw_free(freq_repr);
 }
 
-
-void normalize(float* tab) {
-    float* p = tab;
-    while (*p++) {
-        if (*p > 0) {
-            *p = roundf(logf(*p));
+/**
+ * @brief normalize a float channel (rounds and truncates)
+ * @param float* tab
+ */
+void normalize(int size, float* tab) {
+    float max = 0.0;
+    for (int i = 0; i < size; i++) {
+        if (max < tab[i]) {
+            max = tab[i];
         }
-        if (*p < 0) {
-            *p = 0;
-        }
-        /*printf("%f\n", *p);*/
+    }
+    if (max == 0.0) max = 1.0;
+    for (int i = 0; i < size; i++) {
+        tab[i] = logf(tab[i]);
+        tab[i] *= 255.0 / logf(max);
     }
 }
 
@@ -132,44 +141,55 @@ void normalize(float* tab) {
 void
 test_display(char* name) {
     fprintf(stderr, "test_display: ");
-    unsigned short* g_ims;
-    fftw_complex* freq_repr;
-    float* as, * ps;
 
-    pnm ims = pnm_load(name);
+    // Loading inputs
+    pnm ims = pnm_load(name); // Alloc
     int rows = pnm_get_height(ims);
     int cols = pnm_get_width(ims);
-    pnm imd_as = pnm_new(cols, rows, PnmRawPpm); // amplitude
-    pnm imd_ps = pnm_new(cols, rows, PnmRawPpm); // phase
-    g_ims = pnm_get_channel(ims, NULL, 0);
+    pnm imd_as = pnm_new(cols, rows, PnmRawPpm); // Alloc
+    pnm imd_ps = pnm_new(cols, rows, PnmRawPpm); // Alloc
+    unsigned short* g_ims = pnm_get_channel(ims, NULL, 0); // Alloc
 
-    freq_repr = forward(rows, cols, g_ims);
-    as = malloc(rows * cols * sizeof(float));
-    ps = malloc(rows * cols * sizeof(float));
+    // Computing result
+    fftw_complex* freq_repr = forward(rows, cols, g_ims); // Alloc
+    float* as = malloc(rows * cols * sizeof(float)); // Alloc
+    float* ps = malloc(rows * cols * sizeof(float)); // Alloc
     freq2spectra(rows, cols, freq_repr, as, ps);
 
-    normalize(as);
-    normalize(ps);
-    unsigned short* _as = (unsigned short*) as;
-    unsigned short* _ps = (unsigned short*) ps;
-    
+    // Normalization of the result
+    normalize(rows * cols, as);
+    normalize(rows * cols, ps);
+
+    // Conversion to int
+    unsigned short* _as = malloc(rows * cols * sizeof(unsigned short*));
+    unsigned short* _ps = malloc(rows * cols * sizeof(unsigned short*));
+    for (int i = 0; i < rows * cols; i++) {
+        _as[i] = as[i];
+        _ps[i] = ps[i];
+    }
+
+    // Filling results
     for (int k = 0; k < 3; k++) {
         pnm_set_channel(imd_as, _as, k);
         pnm_set_channel(imd_ps, _ps, k);
     }
 
+    // Saving results
     char* _name = basename(name);
     char as_name[NAME_SIZE] = "AS-";
     char ps_name[NAME_SIZE] = "PS-";
     pnm_save(imd_as, PnmRawPpm, strcat(as_name, _name));
     pnm_save(imd_ps, PnmRawPpm, strcat(ps_name, _name));
 
+    // Memory free
     pnm_free(imd_as);
     pnm_free(imd_ps);
     pnm_free(ims);
     free(g_ims);
     free(as);
+    free(_as);
     free(ps);
+    free(_ps);
     fftw_free(freq_repr);
 
     fprintf(stderr, "OK\n");
@@ -209,7 +229,7 @@ test_add_frequencies(char* name) {
     while (*p_as++) {
         max = *p_as > max ? *p_as : max;
     }
-    printf("%f\n", max);
+    //printf("%f\n", max);
     int mid_rows = rows / 2;
     int mid_cols = cols / 2;
     as[cols * mid_rows - mid_cols] = 2 * 0.25 * max;
@@ -246,7 +266,7 @@ run(char* name) {
     //test_forward_backward(name);
     //test_reconstruction(name);
     test_display(name);
-    test_add_frequencies(name);
+    // test_add_frequencies(name);
 }
 
 void
